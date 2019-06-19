e13dbc4820581fea5cb5ad582c444cf358b77280
Joshua Anthony <jantho1990@gmail.com>
2019-06-18 21:34:16 -0500

    chest can now face in one of four possible directions

--- entities/chest/Chest.gd
+++ entities/chest/Chest.gd
@@ -7,12 +7,31 @@ const animation_frames = {
 	'east': [54, 55, 56]
 }
 
+var direction_names = {
+	Vector2(0, -1): 'north',
+	Vector2(-1, 0): 'west',
+	Vector2(0, 1): 'south',
+	Vector2(1, 0): 'east'
+}
+
 var direction = Vector2(1, 0)
+
+export(String, 'north', 'west', 'south', 'east') var direction_name = 'north'
+
+onready var Animator = $Sprite/AnimationPlayer
+var direction_vectors = {
+	'north': Vector2(0, -1),
+	'west': Vector2(-1, 0),
+	'south': Vector2(0, 1),
+	'east': Vector2(1, 0)
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	direction = direction_vectors[direction_name]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var anim = direction_names[direction] + '_idle'
+	Animator.play(anim)
+	pass
